20cee94ae07f5922e08b0d924d61c843312e4d6f
A.M. Simon <145704643+writethings-whirlpool@users.noreply.github.com>
2023-10-13 22:26:57 +0800

    Create accompanying.js

---
+++ accompanying.js
@@ -0,0 +1,12 @@
+function hello() {
+  alert("This is a little fiddle sample to show that I know a little bit of html, css and javascript :)");
+}
+
+function showtime() {
+  var now = new Date();
+  alert(now);
+}
+
+function lechat() {
+  alert ("[Too much catnip can cause health problems in cats, such as vomiting, diarrhea, dizziness, or having trouble walking. Use just a little at a time, and you can always discuss the correct amount for your cat with your veterinarian.] - Petmd.com");
+}
